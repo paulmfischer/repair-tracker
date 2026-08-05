64110ded532a0cfa7871d6ac354af2f21f1f8820
Paul Fischer <me@paulmfischer.dev>
2026-08-05 07:22:43 -0500

    fix: pass git commit to WASM client via meta tag for version display

--- RepairTracker.Server/wwwroot/js/appLoading.js
+++ RepairTracker.Server/wwwroot/js/appLoading.js
@@ -1,3 +1,7 @@
 window.hideAppLoadingScreen = function () {
     document.getElementById('app-loading')?.remove();
 };
+
+window.getGitCommitMetaTag = function () {
+    return document.querySelector('meta[name="git-commit"]')?.getAttribute('content') ?? '';
+};
